04fb0b3567a4ce39c7c054ce63b2d084abea2094
Andrews Medina <andrewsmedina@gmail.com>
2013-09-17 18:56:24 -0300

    using mysql.

--- simple/simple/settings.py
+++ simple/simple/settings.py
@@ -11,13 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE_NAME', 'todo')
+        'USER': os.environ.get('MYSQL_USER', 'root')
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', '')
+        'HOST': os.environ.get('MYSQL_HOST', '')
+        'PORT': os.environ.get('MYSQL_PORT', ''),
     }
 }
 
