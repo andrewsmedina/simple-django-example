2390c65a2a2ac99916a914f654a41db8c0e1c0a9
Andrews Medina <andrewsmedina@gmail.com>
2013-09-17 16:41:23 -0300

    added todo admin.

---
+++ todo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Todo
+
+
+admin.site.register(Todo)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.task
