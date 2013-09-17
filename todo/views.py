3cd056edb6a611015921877c4b2bd0795014dff5
Andrews Medina <andrewsmedina@gmail.com>
2013-09-17 16:46:22 -0300

    added add todo.

--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,11 @@ from .models import Todo
 def list(request):
     todos = Todo.objects.all()
     return render(request, {"todos": todos}, "list.html")
+
+
+def add(request):
+    form = TodoForm(request.POST or {})
+    if form.is_valid():
+        form.save()
+        return redirect("/")
+    return render(request, {"form": form}, "add.html")
